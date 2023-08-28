ad646e915d87b6311880bb57a954ec6e0337e34d
Filippo-Angaroni <angaronifilippo@gmail.com>
2023-08-28 12:22:22 +0200

    Update

--- Ufficio 10/Ufficio.Designer.cs
+++ Ufficio 10/Ufficio.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(517, 192);
+            button1.Location = new Point(23, 23);
             button1.Name = "button1";
             button1.Size = new Size(155, 95);
             button1.TabIndex = 0;
